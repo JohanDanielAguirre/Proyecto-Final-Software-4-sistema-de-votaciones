e04f82c61d513b5448bdb29d403ab9ad699f82e1
alejoamu <alejoamu2@gmail.com>
2024-12-01 17:48:56 -0500

    first broker version

--- distribuido/distribuido/server/src/main/java/PrinterI.java
+++ distribuido/distribuido/server/src/main/java/PrinterI.java
@@ -1,11 +1,6 @@
 import Demo.Response;
 import com.zeroc.Ice.Current;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 public class PrinterI implements Demo.Printer {
 
     @Override
@@ -17,46 +12,7 @@ public class PrinterI implements Demo.Printer {
     }
 
     private String executeQuery(String documento) {
-        String query = """
-                SELECT c.nombre AS ciudadano_nombre,c.apellido AS ciudadano_apellido, c.documento AS ciudadano_documento,d.nombre AS departamento,
-               m.nombre AS municipio,pv.direccion AS puesto_direccion, mv.id AS mesa
-               FROM ciudadano c
-               JOIN mesa_votacion mv ON c.mesa_id = mv.id
-                JOIN puesto_votacion pv ON mv.puesto_id = pv.id
-                JOIN municipio m ON pv.municipio_id = m.id
-                JOIN departamento d ON m.departamento_id = d.id
-                WHERE c.documento = ?
-        """;
-
-        StringBuilder result = new StringBuilder();
-        try (Connection connection = new DatabaseConnector().connect();
-             PreparedStatement stmt = connection.prepareStatement(query)) {
-            //System.out.println("Entra a formar la query");
-            stmt.setString(1, documento);
-            try (ResultSet rs = stmt.executeQuery()) {
-                //System.out.println("Entra a uniendo la query");
-                if (rs.next()) {
-                    result.append("El ciudadano ")
-                            .append(rs.getString("ciudadano_nombre")).append(" ")
-                            .append(rs.getString("ciudadano_apellido"))
-                            .append(" identificado con el documento ")
-                            .append(rs.getString("ciudadano_documento"))
-                            .append(" debe votar en ")
-                            .append(rs.getString("departamento")).append(", ")
-                            .append(rs.getString("municipio")).append(", ")
-                            .append("En la direccion ")
-                            .append(rs.getString("puesto_direccion"))
-                            .append(". En la mesa ")
-                            .append(rs.getInt("mesa")).append(".");
-                } else {
-                    result.append("No se encontró ningún ciudadano con el documento ").append(documento).append(".");
-                }
-            }
-        } catch (SQLException e) {
-            result.append("Error ejecutando consulta: ").append(e.getMessage());
-        }
-        System.out.println("query result: " + result.toString());
-        return result.toString();
+        // Simular una respuesta en lugar de realizar una consulta a la base de datos
+        return "Simulación: El ciudadano con documento " + documento + " debe votar en el departamento simulado, municipio simulado, en la dirección simulada. En la mesa simulada.";
     }
-
 }
